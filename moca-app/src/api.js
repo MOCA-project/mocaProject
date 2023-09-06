f8f8cbe6ddf25048637801b610a34208dfb19374
rmsouza <raul.souza@sptech.school>
2023-09-05 21:59:11 -0300

    alteracao de porta

--- moca-app/src/api.js
+++ moca-app/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL: "http://localhost:8080/api/"
-    baseURL: "https://moca-rest-service.azurewebsites.net/api/"
+    baseURL: "http://54.225.15.170:3000/api/"
 })
 
 export default api;
