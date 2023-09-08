49fdaa7715e4990d03f7ef21e927e064d8e1979a
DiogoNMM <diogo.martinez@sptech.school>
2023-09-08 00:12:49 -0300

    restauracao de porta AWS

--- moca-app/src/api.js
+++ moca-app/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL: "http://localhost:8080/api/"
-    baseURL: "http://54.225.15.170:3000/api/"
+    baseURL: "http://54.225.15.170:8080/api/"
 })
 
 export default api;
