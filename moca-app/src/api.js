398617ad17fbd1e14e00e50cd179dec7f1ed83c7
DiogoNMM <diogo.martinez@sptech.school>
2023-10-22 02:15:44 -0300

    ajuste porta

--- moca-app/src/api.js
+++ moca-app/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
     // baseURL: "http://localhost:8080/api/"
     // baseURL: "http://54.225.15.170:8080/api/"
-    baseURL: "https://54.225.15.170:8443/api"
+    baseURL: "https://54.225.15.170:8443/api/"
 })
 
 export default api;
